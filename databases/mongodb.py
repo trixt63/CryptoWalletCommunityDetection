c1725d743355e6c40d72fee987913fdc6bf695d7
Trinh Xuan Tung <xuantung.trinh.63@gmail.com>
2023-05-05 17:00:24 +0700

    change collection name to cryptowallets

--- databases/mongodb.py
+++ databases/mongodb.py
@@ -16,7 +16,7 @@ class MongoDB:
         self.connection = MongoClient(connection_url)
 
         self._db = self.connection[MongoDBConfig.DATABASE]
-        self.wallets_col = self._db['wallets']
+        self.wallets_col = self._db['cryptowallets']
 
         self._create_index()
 
