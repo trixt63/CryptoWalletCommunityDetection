0b7f3ddd933cdea2702b643b460319448dccc695
Trinh Xuan Tung <xuantung.trinh.63@gmail.com>
2023-05-15 02:14:13 +0700

    change default interval from hour to day

--- cli/dex_wallets.py
+++ cli/dex_wallets.py
@@ -11,7 +11,7 @@ logger = get_logger('Exchange Trading Enricher')
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 # @click.option('-c', '--chain', default='bsc', show_default=True, type=str, help='Network name example bsc or polygon')
-@click.option('--interval', default=TimeConstants.A_HOUR, show_default=True, type=int, help='Interval to repeat execute')
+@click.option('--interval', default=TimeConstants.A_DAY, show_default=True, type=int, help='Interval to repeat execute')
 def dex_wallets(interval):
     """Get exchange trading information."""
     chain_ids = ['0xfa', '0x1', '0x38']
