ba5d6cd4a3317f5b9dd4a6fabe45cd5e0713627e
Trinh Xuan Tung <xuantung.trinh.63@gmail.com>
2023-05-15 14:21:26 +0700

    modify mongodb update_wallets: add update nested documents

--- databases/mongodb.py
+++ databases/mongodb.py
@@ -6,6 +6,7 @@ from utils.logger_utils import get_logger
 from utils.format_utils import snake_to_lower_camel
 
 logger = get_logger('MongoDB')
+WALLETS_COL = 'dexTraders'
 
 
 class MongoDB:
@@ -18,7 +19,7 @@ class MongoDB:
 
         self._db = self.connection[MongoDBConfig.DATABASE]
         self.lp_tokens_col = self._db['elite_lp_tokens']
-        self.wallets_col = self._db['dexTraders']
+        self.wallets_col = self._db[WALLETS_COL]
 
         self._create_index()
 
@@ -27,20 +28,34 @@ class MongoDB:
             self.wallets_col.create_index([('number_of_txs', 1)], name='wallets_number_of_txs_index_1')
 
     def update_wallets(self, wallets: List[dict]):
-
         try:
-            wallets_update = []
+            wallet_updates_bulk = []
             for wallet in wallets:
-                wallet_mongo = {
-                    snake_to_lower_camel(_key): _val
-                    for _key, _val in wallet.items()
+                wallet['_id'] = wallet['address']
+                # pop all information besides data about lendings/dex/deposit/...
+                wallet_base_data = {
+                    '_id': wallet.pop('_id'),
+                    'address': wallet.pop('address'),
                 }
-                wallet_mongo['_id'] = wallet['address']
-                tags = wallet_mongo.pop('tags')
-                wallets_update.append(UpdateOne({'_id': wallet_mongo['_id']},
-                                                {'$set': wallet_mongo, '$addToSet': {"tags": {'$each': tags}}},
-                                                upsert=True))
-            self.wallets_col.bulk_write(wallets_update)
+                tags = wallet.pop('tags')
+
+                # process query to update nested documents for data about lendings/dex/deposit/...
+                field_name = list(wallet.keys())[0]  # 'lendingPools' or 'tradedLPs' or 'exchangesDeposited'...
+                project_names = list(wallet[field_name].keys())  # project id
+                # update nested documents
+                _mongo_add_to_set_query = {f"{field_name}.{project_name}": {"$each": wallet[field_name][project_name]}
+                                           for project_name in project_names}
+                _mongo_add_to_set_query["tags"] = {'$each': tags}
+
+                # add update query into bulk
+                _filter = {'_id': wallet_base_data['_id']}
+                _update = {
+                    '$set': wallet_base_data,
+                    '$addToSet': _mongo_add_to_set_query
+                }
+                wallet_updates_bulk.append(UpdateOne(filter=_filter, update=_update, upsert=True))
+
+            self.wallets_col.bulk_write(wallet_updates_bulk)
         except Exception as ex:
             logger.exception(ex)
 
@@ -48,3 +63,55 @@ class MongoDB:
     #     _filter = {'chainId': chain_id}
     #     tokens_cursors = self.lp_tokens_col.find(_filter).batch_size(1000)
     #     return tokens_cursors
+
+    # def test_update(self):
+    #     datum = {
+    #       "_id": "0x305246818d727167f028f14b6f631e92a8e15795",
+    #       "address": "0x305246818d727167f028f14b6f631e92a8e15795",
+    #       "tags": [
+    #         "dex_trader",
+    #         "bot"
+    #       ],
+    #       "tradedlps": {
+    #         "pancakeswap": [
+    #           {
+    #             "chainId": "0x38",
+    #             "address": "0x2222"
+    #           }
+    #         ],
+    #         "spookyswap": [
+    #             {
+    #                 "chainId": "0xfa",
+    #                 "address": "0x1111"
+    #             },
+    #             {
+    #                 "chainId": "0xfa",
+    #                 "address": "0x2222"
+    #             }
+    #         ]
+    #       }
+    #     }
+    #
+    #     tags = datum.pop('tags')
+    #     base_datum = {
+    #         '_id': datum.pop('_id'),
+    #         'address': datum.pop('address'),
+    #     }
+    #
+    #     field_name = list(datum.keys())[0]
+    #     platform_names = list(datum[field_name].keys())
+    #
+    #     _filter = {'_id': base_datum['_id']}
+    #
+    #     __add_to_set = {f"{field_name}.{platform_name}": {"$each": datum[field_name][platform_name]}
+    #                     for platform_name in platform_names}
+    #     __add_to_set["tags"] = {'$each': tags}
+    #     _update = {
+    #         '$set': base_datum,
+    #         '$addToSet': __add_to_set
+    #     }
+    #     self.wallets_col.update_one(filter=_filter,
+    #                                 update=_update,
+    #                                 upsert=True)
+    #
+    #     return 0
