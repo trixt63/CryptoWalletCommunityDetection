cb096ac2b4eac3e9dd2cd4d934f2b6f0cc095364
Trinh Xuan Tung <xuantung.trinh.63@gmail.com>
2023-05-23 10:16:12 +0700

    add csv files for CEX wallets

--- models/wallet/wallet_deposit_exchange.py
+++ models/wallet/wallet_deposit_exchange.py
@@ -8,21 +8,21 @@ from models.project import Project
 class WalletDepositExchange(Wallet):
     def __init__(self, address):
         super().__init__(address)
-        self.deposited_exchanges: Dict[str, Project] = dict()
+        self.exchanges_deposited: Dict[str, Project] = dict()
         self.add_tags(WalletTags.centralized_exchange_deposit_wallet)
 
     def add_project(self, project: Project):
-        if project.project_id in self.deposited_exchanges:
-            self.deposited_exchanges[project.project_id].add_deployments(project.deployments)
+        if project.project_id in self.exchanges_deposited:
+            self.exchanges_deposited[project.project_id].add_deployments(project.deployments)
         else:
-            self.deposited_exchanges[project.project_id] = project
+            self.exchanges_deposited[project.project_id] = project
 
     def to_dict(self):
         returned_dict = super().to_dict()
 
-        if len(self.deposited_exchanges):
+        if len(self.exchanges_deposited):
             deposited_exchanges = {cex_id: chains.to_deployed_chains_list()
-                                   for cex_id, chains in self.deposited_exchanges.items()}
+                                   for cex_id, chains in self.exchanges_deposited.items()}
             returned_dict['depositedExchanges'] = deposited_exchanges
 
         return returned_dict
