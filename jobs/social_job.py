340c946688540109da132d891522476f114e010e
Trinh Xuan Tung <xuantung.trinh.63@gmail.com>
2023-07-05 17:08:40 +0700

    small scripts to aggregate user and social account

--- jobs/social_job.py
+++ jobs/social_job.py
@@ -11,7 +11,7 @@ logger = get_logger('Social Job')
 def main():
     # vi_dac_biet = '0x95106a95922a179f4b951e8c886952498afe8f19'
     mongodb = MongoDB()
-    mongodb.update_cex_users()
+    # mongodb.update_cex_users()
 
 
 if __name__ == "__main__":
